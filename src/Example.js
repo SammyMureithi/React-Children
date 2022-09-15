7ed39e79fdd9190a3f5ee264b6018a9d283e54a9
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-15 10:58:57 +0300

    React children

--- src/Example.js
+++ src/Example.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Example(props) {
   return (
       <div>
-          <h1>{props.title }</h1>
+          <h1>{props.title}</h1>
+          {props.children}
     </div>
   )
 }

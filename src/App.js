f096aa722deb7527d2b2be2c099636724dae80d4
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-15 10:55:44 +0300

    React children

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Example title="React Children">
+        <p>Am a child from example</p>
+        <p>Am also a Child not a prop</p>
+      </Example>
+      <Example title="Example2 React Children">
+        <p>You can have anything displayed in the example children</p>
+        <p>We have had anything we wanted displayed in our children props</p>
+      </Example>
     </div>
   );
 }
---
+++ src/Example.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Example(props) {
+  return (
+      <div>
+          <h1>{props.title }</h1>
+    </div>
+  )
+}
+
+export default Example
